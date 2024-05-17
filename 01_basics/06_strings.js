814b2c2d546149e8de1227389fae3508131ef8d5
ArpitKatiyar <arpitkatiyar09197@gmail.com>
2024-05-17 12:32:31 +0530

    Add Comments

--- 01_basics/06_strings.js
+++ 01_basics/06_strings.js
@@ -1,17 +1,17 @@
-// const name="Arpit"
-// console.log(name[0])
+const name="Arpit"
+console.log(name[0]) // A
 
-// const repoCount=50
+const repoCount=50
 
-// console.log((`Hello my name is ${name} and my repo count is ${repoCount}`))
+console.log((`Hello my name is ${name} and my repo count is ${repoCount}`)) // Hello my name is Arpit and my repo count is 50
 
 
-// const gameName=new String('hitesh')
-// console.log(gameName[0])
+const gameName=new String('hitesh')
+console.log(gameName[0])
 
-// console.log(gameName.toUpperCase())  //HITESH
-// console.log(gameName.indexOf('t')) // 2
-// console.log(gameName.substring(0,4)) // hite
+console.log(gameName.toUpperCase())  //HITESH
+console.log(gameName.indexOf('t')) // 2
+console.log(gameName.substring(0,4)) // hite
 
 const newStr=" Arpan "
 console.log(newStr.trim()) // Arpan
